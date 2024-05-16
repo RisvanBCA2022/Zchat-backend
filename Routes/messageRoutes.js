13bef379b4a619611c5a01b6a610c1c57894f618
RisvanBCA2022 <mohammedrisvan16@gmail.com>
2024-05-16 14:20:55 +0530

    jwt verification and message routes renamed to userRoutes

--- Routes/messageRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { sendFriendRequest } from '../Controllers/userController';
-const router=express.Router();
-
-router.post('/message',sendFriendRequest);
-
-export default router;  
--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,15 +1,7 @@
-import express from 'express'
-import userSchema from '../Model/userSchema.js'
-const router=express.Router()
+import express from 'express';
+import { sendFriendRequest } from '../Controllers/userController';
+const router=express.Router();
 
-router.get('/gettotalusers',async (req,res)=>{
-    try {
-        const response= await userSchema.countDocuments()
-        console.log(response)
-         res.status(200).json({data:response})
-    } catch (error) {
-        res.send(error.message)
-    }
-})
+router.post('/sendrequest',sendFriendRequest);
 
-export default router
+export default router;  
