23ecaecfbf6a863b64c99101dad4f8994235eb09
RisvanBCA2022 <mohammedrisvan16@gmail.com>
2024-05-18 15:42:44 +0530

    Email verify Tested

--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express'
-import { login, signup } from '../Controllers/authControllers.js'
+import { login, signup, verifyEmail } from '../Controllers/authControllers.js'
 
 const router=express.Router()
 
 router.post('/signup',signup)
 router.post('/login',login)
-
+router.post('/verify-email', verifyEmail);
 export default router
