99fc7ba569cee56076c6684b4c0a0cd35260871b
RisvanBCA2022 <mohammedrisvan16@gmail.com>
2024-05-21 16:54:17 +0530

    notification markasread added

--- Controllers/notificationController.js
+++ Controllers/notificationController.js
@@ -11,3 +11,16 @@ export const getNotification = async (req, res) => {
     next(error);
   }
 };
+
+export const markAsRead = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    await notificationSchema.updateMany(
+      { receiver: userId },
+      { $set: { read: true } }
+    );
+    res.status(200).json({ success: true });
+  } catch (error) {
+    next(error);
+  }
+}
--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { acceptrequest, sendFriendRequest,rejectrequest, getFriendRequests } from '../Controllers/userController.js';
 import jwtVerify from '../Middleware/jwtVerify.js';
 import { uploadMedia } from '../Controllers/mediaController.js';
-import { getNotification } from '../Controllers/notificationController.js';
+import { getNotification, markAsRead } from '../Controllers/notificationController.js';
 const router=express.Router();
 
 router.post('/sendrequest',jwtVerify,sendFriendRequest);
@@ -11,5 +11,7 @@ router.post('/rejectrequest',jwtVerify,rejectrequest);
 router.get('/getfriendrequests',jwtVerify,getFriendRequests);
 router.post('/uploadmedia',jwtVerify,uploadMedia)
 router.get('/notifications',jwtVerify,getNotification)
+router.post('/nofications/read',jwtVerify, markAsRead)
+
 
 export default router;  
